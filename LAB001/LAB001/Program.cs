aa9a78a39ca27a053e28b70203034263a6ac6d43
PitchakornSKT <pitchakorn.sa@kkumail.com>
2022-11-28 16:05:43 +0700

    pitchakorn

---
+++ LAB001/LAB001/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("พิชชากร สกุลไทย");
+Console.WriteLine("653450095-6");
+Console.WriteLine("pitchakorn.sa@kkumail.com");
